d4db8f1d318953ac54e72c3a406a18c4d350d1ed
Caitlyn Widjaja <widjajacaitlyn@gmail.com>
2020-07-14 10:31:39 -0700

    Iteration 0 Complete

---
+++ ToDoApp/ToDo.swift
@@ -0,0 +1,13 @@
+//
+//  ToDo.swift
+//  ToDoApp
+//
+//  Created by Caitlyn Widjaja on 7/13/20.
+//  Copyright © 2020 Caitlyn Widjaja. All rights reserved.
+//
+
+import UIKit
+class ToDo{
+    var name = ""
+    var important = false 
+}
